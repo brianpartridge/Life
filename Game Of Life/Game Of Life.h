368ddf5dd953ecf31107b0aa546045694c01d385
Brian Partridge <brianpartridge@gmail.com>
2015-10-25 16:50:08 -0400

    Initial Commit

---
+++ Game Of Life/Game Of Life.h
@@ -0,0 +1,19 @@
+//
+//  Game Of Life.h
+//  Game Of Life
+//
+//  Created by Brian Partridge on 10/25/15.
+//  Copyright © 2015 PearTreeLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Game Of Life.
+FOUNDATION_EXPORT double Game_Of_LifeVersionNumber;
+
+//! Project version string for Game Of Life.
+FOUNDATION_EXPORT const unsigned char Game_Of_LifeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Game_Of_Life/PublicHeader.h>
+
+
