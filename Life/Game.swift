6f48a91baaedf96a92362f65e46adf8474c94682
Brian Partridge <brianpartridge@squareup.com>
2015-10-25 23:37:19 -0400

    Add Game model.

---
+++ Life/Game.swift
@@ -0,0 +1,45 @@
+//
+//  Game.swift
+//  Life
+//
+//  Created by Brian Partridge on 10/25/15.
+//  Copyright © 2015 PearTreeLabs. All rights reserved.
+//
+
+import Foundation
+
+
+protocol GameType {
+    func tick()
+}
+
+public struct Game {
+
+    // MARK: - Public Properties
+    
+    public let initialBoard: Board
+    public private(set) var currentBoard: Board
+    public private(set) var tickCount: Int = 0
+    
+    public let rules: [Rule]
+    
+    // MARK: - Initializers
+    
+    public init(board: Board, rules:[Rule]) {
+        self.initialBoard = board
+        self.currentBoard = board
+        self.rules = rules
+    }
+    
+    // MARK: - Public Methods
+    
+    public mutating func tick() {
+        // TODO: (brianpartridge) Evaluate multiple rules at once
+        let rule = rules.last!
+        
+        let board = currentBoard
+        let newCells = board.map { rule.evaluateForCellAtCoordinate($0, inBoard: board) }
+        currentBoard = Board(size: board.size, cells: newCells)
+        tickCount++
+    }
+}
---
+++ Life/GameTests.swift
@@ -0,0 +1,109 @@
+//
+//  GameTests.swift
+//  Life
+//
+//  Created by Brian Partridge on 10/25/15.
+//  Copyright © 2015 PearTreeLabs. All rights reserved.
+//
+
+@testable import Life
+import XCTest
+
+
+class GameTests: XCTestCase {
+
+    let fullBoard = Board.randomBoard(Size(width: 3, height: 3), populationDensity: 1)
+    let emptyBoard = Board.randomBoard(Size(width: 3, height: 3), populationDensity: 0)
+    
+    // MARK: - Tests
+    
+    func test_tick_empty_passthrough() {
+        let rule = TestRule()
+        var game = Game(board: emptyBoard, rules: [rule])
+        game.tick()
+        XCTAssertEqual(game.initialBoard, emptyBoard)
+        XCTAssertEqual(game.currentBoard, emptyBoard)
+        XCTAssertEqual(game.tickCount, 1)
+        game.tick()
+        XCTAssertEqual(game.initialBoard, emptyBoard)
+        XCTAssertEqual(game.currentBoard, emptyBoard)
+        XCTAssertEqual(game.tickCount, 2)
+    }
+    
+    func test_tick_full_passthrough() {
+        let rule = TestRule()
+        var game = Game(board: fullBoard, rules: [rule])
+        game.tick()
+        XCTAssertEqual(game.initialBoard, fullBoard)
+        XCTAssertEqual(game.currentBoard, fullBoard)
+        XCTAssertEqual(game.tickCount, 1)
+        game.tick()
+        XCTAssertEqual(game.initialBoard, fullBoard)
+        XCTAssertEqual(game.currentBoard, fullBoard)
+        XCTAssertEqual(game.tickCount, 2)
+    }
+    
+    func test_tick_empty_conway_dead() {
+        let rule = ConwayRule(edgeHandlingStyle: .Dead)
+        var game = Game(board: emptyBoard, rules: [rule])
+        game.tick()
+        XCTAssertEqual(game.initialBoard, emptyBoard)
+        XCTAssertEqual(game.currentBoard, emptyBoard)
+        XCTAssertEqual(game.tickCount, 1)
+        game.tick()
+        XCTAssertEqual(game.initialBoard, emptyBoard)
+        XCTAssertEqual(game.currentBoard, emptyBoard)
+        XCTAssertEqual(game.tickCount, 2)
+    }
+    
+    func test_tick_empty_conway_wrap() {
+        let rule = ConwayRule(edgeHandlingStyle: .Wrap)
+        var game = Game(board: emptyBoard, rules: [rule])
+        game.tick()
+        XCTAssertEqual(game.initialBoard, emptyBoard)
+        XCTAssertEqual(game.currentBoard, emptyBoard)
+        XCTAssertEqual(game.tickCount, 1)
+        game.tick()
+        XCTAssertEqual(game.initialBoard, emptyBoard)
+        XCTAssertEqual(game.currentBoard, emptyBoard)
+        XCTAssertEqual(game.tickCount, 2)
+    }
+    
+    func test_tick_full_conway_dead() {
+        let rule = ConwayRule(edgeHandlingStyle: .Dead)
+        var game = Game(board: fullBoard, rules: [rule])
+        let firstTickBoard = Board(size: fullBoard.size, cells: [
+            .Alive, .Dead, .Alive,
+            .Dead,  .Dead, .Dead,
+            .Alive, .Dead, .Alive])
+        game.tick()
+        XCTAssertEqual(game.initialBoard, fullBoard)
+        XCTAssertEqual(game.currentBoard, firstTickBoard)
+        XCTAssertEqual(game.tickCount, 1)
+        game.tick()
+        XCTAssertEqual(game.initialBoard, fullBoard)
+        XCTAssertEqual(game.currentBoard, emptyBoard)
+        XCTAssertEqual(game.tickCount, 2)
+    }
+
+    func test_tick_full_conway_wrap() {
+        let rule = ConwayRule(edgeHandlingStyle: .Wrap)
+        var game = Game(board: fullBoard, rules: [rule])
+        game.tick()
+        XCTAssertEqual(game.initialBoard, fullBoard)
+        XCTAssertEqual(game.currentBoard, emptyBoard)
+        XCTAssertEqual(game.tickCount, 1)
+        game.tick()
+        XCTAssertEqual(game.initialBoard, fullBoard)
+        XCTAssertEqual(game.currentBoard, emptyBoard)
+        XCTAssertEqual(game.tickCount, 2)
+    }
+
+}
+
+
+class TestRule: Rule {
+    func evaluateForCellAtCoordinate(coordinate: Coordinate, inBoard board: Board) -> Cell {
+        return board.cellAtCoordinate(coordinate)
+    }
+}
