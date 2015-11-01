764519e8786a40ac3dc813d48dc56e59c880ec03
Brian Partridge <brianpartridge@gmail.com>
2015-11-01 11:43:39 -0500

    Update playground.

--- Example.playground/Contents.swift
+++ Example.playground/Contents.swift
@@ -3,30 +3,17 @@
 import Life
 import XCPlayground
 
+let glider = Glider()
+
 let origin = Coordinate(x: 0, y: 0)
 let size = Size(width: 10, height: 10)
-let board = boardByInsertingGliderAtCoordinate(origin, inBoard: Board.emptyBoard(size))
+let emptyBoard = Board.emptyBoard(size)
+let board = emptyBoard.boardByInsertingPattern(glider, atCoordinate: origin)
 
 let rules = ConwayRuleSet(edgeHandlingStyle: .Dead)
 var game = Game(board: board, rules: rules)
 
+// Make sure to reveal the Timeline in the Assistant Editor 👉🏼
 game.forEach {
     XCPlaygroundPage.currentPage.captureValue($0.description, withIdentifier: "Generation \($0.number)")
-}
-
-
-
-func boardByInsertingGliderAtCoordinate(offset: Coordinate, inBoard board: Board) -> Board {
-    let coordinates = [
-        Coordinate(x: 1, y: 0),
-        Coordinate(x: 2, y: 1),
-        Coordinate(x: 0, y: 2),
-        Coordinate(x: 1, y: 2),
-        Coordinate(x: 2, y: 2),
-    ]
-    let indecies = coordinates.map { board.cellIndexForCoordinate($0 + offset) }
-    
-    var cells = board.cells
-    indecies.forEach { cells[$0] = .Alive }
-    return Board(size: board.size, cells: cells)
 }
